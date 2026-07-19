51d2271f8f3977d014574d41b07952fa6c797ddd
Zeeshan Shaikh <shaikhzeeshan7554@gmail.com>
2026-07-19 17:16:47 +0530

    Set up basic Flask application with homepage

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
-from utils.data_loader import load_students
-from analysis.student_analysis import at_risk_students, class_summary, top_students
-from analysis.student_analysis import subject_average
-import visualization.charts as charts
-from analysis.analytics import dashboard_kpis
+from flask import Flask, render_template
 
-students = load_students()
+app = Flask(__name__)
 
-class_summary(students)
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-subject_average(students)
-
-top_students(students)
-
-at_risk_students(students)
-
-charts.subject_average_chart(students)
-
-charts.attendance_distribution_chart(students)
-
-charts.sgpi_distribution_chart(students)
-
-charts.top_students_chart(students)
-
-dashboard_kpis(students)
-
+if __name__ == "__main__":
+    app.run(debug=True)

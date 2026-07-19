fa4e95dac7fdd431c2947ba646bc7d74ad890447
Zeeshan Shaikh <shaikhzeeshan7554@gmail.com>
2026-07-19 17:29:12 +0530

    Connect Flask dashboard with dynamic KPI data

--- analysis/analytics.py
+++ analysis/analytics.py
@@ -1,15 +1,27 @@
 def dashboard_kpis(df):
-    total_students = len(df)
-    average_sgpi = round(df['SGPI'].mean(), 2)
-    average_attendance = round(df['Attendance'].mean(), 2)
-    highest_sgpi = df['SGPI'].max()
-    lowest_sgpi = df['SGPI'].min()
-    at_risk = len(df[(df['Attendance'] < 75) | (df['SGPI'] < 6)])
 
-    print("\n========== DASHBOARD KPI ==========")
-    print(f"Total Students: {total_students}")
-    print(f"Average SGPI: {average_sgpi}")
-    print(f"Average Attendance: {average_attendance}%")
-    print(f"Highest SGPI: {highest_sgpi}")
-    print(f"Lowest SGPI: {lowest_sgpi}")
-    print(f"At Risk Students: {at_risk}")
+    total_students = len(df)
+
+    average_sgpi = round(df["SGPI"].mean(), 2)
+
+    average_attendance = round(df["Attendance"].mean(), 2)
+
+    highest_sgpi = df["SGPI"].max()
+
+    lowest_sgpi = df["SGPI"].min()
+
+    at_risk = len(
+        df[
+            (df["Attendance"] < 75) |
+            (df["SGPI"] < 6)
+        ]
+    )
+
+    return {
+        "total_students": total_students,
+        "average_sgpi": average_sgpi,
+        "average_attendance": average_attendance,
+        "highest_sgpi": highest_sgpi,
+        "lowest_sgpi": lowest_sgpi,
+        "at_risk": at_risk
+    }
--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
 
+from utils.data_loader import load_students
+from analysis.analytics import dashboard_kpis
+
 app = Flask(__name__)
+
+students = load_students()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    kpis = dashboard_kpis(students)
+
+    return render_template(
+        "index.html",
+        total_students=kpis["total_students"],
+        average_sgpi=kpis["average_sgpi"],
+        average_attendance=kpis["average_attendance"],
+        at_risk=kpis["at_risk"]
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
